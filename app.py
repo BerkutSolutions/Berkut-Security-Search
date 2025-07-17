05b0e3b03209c6bc162a39c2af7bcd8c043ac396
Aleksandr <keliberda99@yandex.ru>
2025-07-17 21:36:34 +0800

    v1.0.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#主# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 # If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from flask import Flask, request, render_template, redirect
